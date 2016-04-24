e2323288013e3c9f7c96927bf25fd49b756c32da
scottruona <git@scottruona.com>
2016-04-23 21:28:11 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(name="recastpy",
+      version='0.1',
+      py_modules=["recastpy"],
+      description="A minimal Python wrapper for the Recast.AI API",
+      author="Scott Ruona",
+      author_email="git@scottruona.com",
+      url="https://github.com/scottruona/recastpy",
+      long_description=open('README.md').read()
+     )
